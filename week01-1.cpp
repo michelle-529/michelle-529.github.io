3db33dea4c9b37851ca61036468e8b455a106441
michelle-529 <michelle10526@gmail.com>
2025-02-21 12:01:50 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,14 @@
+///week01-1.cpp
+///在瘋狂程設(練習模式)寫好的程式碼，全選，Ctrl-C
+///CodeBlocks File-New-Empty空白檔案，Ctrl-S存檔
+#include<stdio.h> ///使用standard標準的input/output
+
+int main()
+{
+
+	int a,b; ///整數a,b
+	scanf("%d%d",&a,&b);
+	///兩個%d%d  &  &
+	printf("%d",a+b);
+
+	}
